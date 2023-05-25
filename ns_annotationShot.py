097bd2ab8f41a6233f0ea1df0c88c5e11c695c2f
dansturm-sandwich <dan@sandwich.co>
2023-05-25 10:14:36 -0700

    v02

--- ns_annotationShot.py
+++ ns_annotationShot.py
@@ -3,6 +3,8 @@ import hiero.ui
 import os
 import os.path 
 import datetime
+from PySide2 import QtCore
+from PySide2.QtCore import *
 
 
 
@@ -14,6 +16,7 @@ def annotationShot():
 		seq = ve.sequence()       	# get name of sequence in viewer
 		tia = seq.trackItemAt(vti)    # get track item in sequence at playhead
 
+		timg = te.image()			# get viewer image
 
 		tts = tia.mapTimelineToSource(vti)+1  # map timeline time to source clip plus one because start frame 
 		tts = int(tts)			# get frame as an integer (no decimals)
@@ -37,15 +40,36 @@ def annotationShot():
 		fpth = opt + "/_notes/" + fdte + "/" + fin + ".png"		# full path + filename and extension
 		jpth = opt + "/_notes/" + fdte + "/"	# just the full path, no filename
 
+		fmt = seq.format()                      # format of sequence
+		swid = fmt.width()                      # width of sequence
+		shig = fmt.height()                     # height of sequence
+
+
+		vsize = timg.size()                     # viewer image size
+		vwid = vsize.width()                    # viewer image width
+		vhig = vsize.height()                   # viewer image height
+
+		svwid = vwid*(shig/vhig)				# scaled width of viewer
+
+
+		ssx = (svwid/2) - (swid/2)				# position of left edge of frame
+
+		simg = timg.scaledToHeight(shig, mode=Qt.SmoothTransformation)      # scale viewer image to format height
+
+		cimg = simg.copy(ssx, 0, swid, shig)  	# crop image to format
+
 
 		dircmd = 'mkdir -p "' + jpth + '"'		# if it doesn't exist, make dated folder inside _notes folder
-		scapcmd = 'screencapture -R317,148,1930,1090 "' + fpth + '"' 	# caputre the image to the new path
-
 		os.system(dircmd)				# run mkdir command
-		os.system(scapcmd)				# run screencapture command
+
+		cimg.save(fpth, "PNG")			# save cropped & scaled image to folder
+		
+		# add the colorsync profile
+		os.system( 'sips -s profile /Library/ColorSync/Profiles/Displays/StudioDisplay-7B124C67-2DD2-8F2D-1452-F1C958A0C9F4.icc "' + fpth + '"')
+
 		os.system('open "' + jpth + '"')	# open the folder containing the screenshot
 		os.system('open "' + fpth + '"')	# open the screenshot
 
-		print(jpth)
+
 		print(fpth)
 
